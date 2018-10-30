7f519b2c1159503a5aefded708713b42b0719a8b
Itsolution-git <roy.smith0820@gmail.com>
2018-10-30 17:43:50 -0500

    fix for deploy

--- client/src/shared/constants.js
+++ client/src/shared/constants.js
@@ -1,8 +1,8 @@
 let url;
 if (window.location.hostname.indexOf('localhost') != -1)
 	url = 'http://localhost:3000'
-if (window.location.hostname.indexOf('18.222.107.103') != -1)
-	url = 'http://18.222.107.103:3000'
+if (window.location.hostname.indexOf('http://34.230.43.215') != -1)
+	url = 'http://http://34.230.43.215:3000'
 if (window.location.hostname.indexOf('18.191.231.106') != -1)
 	url = 'http://18.191.231.106:3000'
 export default {
