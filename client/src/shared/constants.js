a8e5d593d0255a5a54a97665e61680ab1726423b
Itsolution-git <roy.smith0820@gmail.com>
2018-10-30 17:48:46 -0500

    fix https

--- client/src/shared/constants.js
+++ client/src/shared/constants.js
@@ -5,6 +5,8 @@ if (window.location.hostname.indexOf('34.230.43.215') != -1)
 	url = 'http://34.230.43.215:3000'
 if (window.location.hostname.indexOf('18.191.231.106') != -1)
 	url = 'http://18.191.231.106:3000'
+else
+	url = 'http://34.230.43.215:3000'
 export default {
   API_BASE_URL: url
 }
