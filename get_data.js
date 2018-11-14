144ff6dba20e9a7212834732b31fc4137146d496
Itsolution-git <roy.smith0820@gmail.com>
2018-11-14 04:04:24 -0500

    Add changes

--- get_data.js
+++ get_data.js
@@ -415,7 +415,7 @@ async function getData() {
 
 var cron = require('node-cron');
 
-cron.schedule('*/30 * * * *', () => {
-  console.log('running a task every 5 mins');
+cron.schedule('*/10 * * *', () => {
+  console.log('running a task every 10 hr');
   getData();
 });
