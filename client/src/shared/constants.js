653e47076b41c9ccfbc90c70eb465d104b07244f
Itsolution-git <roy.smith0820@gmail.com>
2018-11-29 14:16:34 -0500

    Add changes

--- client/src/shared/constants.js
+++ client/src/shared/constants.js
@@ -1,13 +1,7 @@
-let url;
-if (window.location.hostname.indexOf('localhost') != -1)
-	url = 'http://localhost:3000'
-// else if (window.location.hostname.indexOf('34.230.43.215') != -1)
-// 	url = 'http://34.230.43.215:3000'
-// else if (window.location.hostname.indexOf('18.191.231.106') != -1)
-// 	url = 'http://18.191.231.106:3000'
-else
-	url = 'http://18.212.1.40:3000'
+var protocol = location.protocol;
+var slashes = protocol.concat("//");
+var host = slashes.concat(location.hostname+':'+location.port);
 export default {
-  API_BASE_URL: url
+  API_BASE_URL: host
 }
 	
